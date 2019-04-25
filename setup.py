36cfb6c5b5c20e31b158fb5704404305a6c2fad0
James Irving <james.irving.phd@outlook.com>
2019-04-25 12:27:08 -0400

    version 0.1.2 created, added functions from Mike, added install dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="JMI_MVM",
-    version="0.1.1",
+    version="0.1.2",
     author="James M. Irving, Michael V. Moravetz",
     author_email="james.irving.phd@outlook.com",
     description="A collection of our functions and classes from bootcamp. ",
@@ -13,9 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jirvingphd/JMI_MVM",
     packages=setuptools.find_packages(),
+    install_requires=['numpy','pandas','seaborn','matplotlib','sklearn','pydotplus'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
 )
